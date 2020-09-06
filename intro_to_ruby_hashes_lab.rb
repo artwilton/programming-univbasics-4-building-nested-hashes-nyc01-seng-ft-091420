da249f92561f19f22cb3688e105c226cfde58581
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-06 16:12:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,9 @@ def monopoly_with_third_tier
         four_pieces_owned: 100},
         names: {
           reading_railroad: {},
-          
+          pennsylvania_railroad: {},
+          b_and_o_railroad: {},
+          shortline_railroad: {}
         }
         }
       }
