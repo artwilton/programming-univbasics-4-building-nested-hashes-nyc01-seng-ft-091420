e80bef06725ec49f160ffefd64cfd326f68c39c3
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-06 16:09:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,8 @@ end
 
 def monopoly_with_third_tier
   hash = {
-    railroads:
-    {pieces: 4,
+    railroads:{
+      pieces: 4,
       rent_in_dollars: {
         one_piece_owned: 25,
         two_pieces_owned: 50,
